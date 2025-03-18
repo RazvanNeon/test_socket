6f1fc1596eb7e510b708777dc697af9f1e40defd
RazvanNeon <craciun.razvan.neon@gmail.com>
2025-03-18 10:09:54 +0200

    Create test_socket.py

---
+++ test_socket.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+import os
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@app.route('/')
+def index():
+    return "Serverul Flask cu WebSocket este în funcțiune!"
+
+@socketio.on('message')
+def handle_message(message):
+    print('Mesaj primit:', message)
+    emit('response', f"Salut! Ai trimis: {message}")
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
